5c15a920da64f9407587bb4635fa801394620b8c
alexandre dorcival <ddorcival@gmail.com>
2021-10-10 14:56:05 +0200

    change deploy name

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:alexandredorc/portfolio.git master:gh-pages
+git push -f git@github.com:alexandredorc/portfolio_v1.git master:gh-pages
 cd -
